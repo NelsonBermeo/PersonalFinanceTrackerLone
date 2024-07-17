6c8628cd4c58f414c5dd452c86afabccfe755320
Nelson Bermeo <141251675+Melson12@users.noreply.github.com>
2024-07-17 07:39:18 -0400

    brain storming

---
+++ concept.py
@@ -0,0 +1,22 @@
+#Well need a main while loop that will always run
+#the options will be to read csv file or exit 
+
+#we can read through the csv file and if it sees a familiar keyword which can be stored in a dict at the top, then it would immediatly add it to the corresponding category, but if not it will call another function called create category that will ask the user to do some stuff 
+
+#dict of familiar payees 
+#{"Amazon" : Items, "Toys R Us" : Alina, "SuperCutz" : Nelson}
+
+#while loop
+
+    #read through (input = filename)
+        #save acc num which is in filename
+        #go through each line
+        #first check if amount is + we can add the information (date, amount, acc num) to a income.csv
+            #move on to next line
+        #else check payee if its in the main dict then we can add the information
+
+
+    #exit 
+
+#Notes/Ideas
+#Amazon is a different story, it would need to be analyzed 
